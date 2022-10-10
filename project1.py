8b3ee5895465120a6d9f0139652903b9ec9e3b17
craftsman91 <g.jurasz@gmail.com>
2022-10-10 23:25:28 +0200

    projekt1-draft

---
+++ project1.py
@@ -0,0 +1,33 @@
+from sys import exit 
+
+def start_point():
+    print("Tutaj startujesz i masz trzy opcie wyboru: ")
+    choice = input("wpisz: opcja A, opcja B lub opcja C \n >")
+    
+
+    if choice == "opcja A":
+        print("Idę do ")
+    elif choice == "opcja B":
+        print("Idę do ")
+    else:
+        dead("Przegrałeś!")
+
+    #
+
+start_point()
+
+
+"""
+def option_a():
+    
+
+def option_b():
+
+def good():
+    
+
+def bad():
+    
+def neutral():
+
+"""
