f9793914ebbd4b5b13a72c625960d6af6dc59fdb
craftsman91 <g.jurasz@gmail.com>
2022-10-26 22:45:50 +0200

    1026

--- ex42.py
+++ ex42.py
@@ -2,48 +2,48 @@
 class Animal(object):
     pass
 
-## ??
+## Dog jest Animal
 class Dog(Animal):
 
     def __init__(self, name):
-        ## ??
+        ## Dog ma name
         self.name = name
 
-## ??
+## Cat jest Animal
 class Cat(Animal):
 
     def __init__(self, name):
-        ## ??
+        ## Cat ma name
         self.name = name
 
-## ??
+## Person jest object
 class Person(object):
 
     def __init__(self, name):
-        ## ??
+        ## Person ma name
         self.name = name
 
         ## Person (osoba) ma pet (jakiegoś zwierzaka)
         self.pet = None
 
-## ?? 
+## Employee jest Person 
 class Employee(Person):
 
     def __init__(self, name, salary):
-        ## ??
+        ## Employee ma name i salary?
         super(Employee, self).__init__(name)
-        ## ??
+        ## Employee ma salary
         self.salary = salary
 
-## ??
+## Fish jest object
 class Fish(object):
     pass
 
-## ??
+## Salmon jest Fish
 class Salmon(Fish):
     pass
 
-## ??
+## Halibut jest Fish
 class Halibut(Fish):
     pass
 
@@ -51,27 +51,27 @@ class Halibut(Fish):
 ## rover jest Dog
 rover = Dog("Rover")
 
-## ??
+## ?? satan jest Cat
 satan = Cat("Satan")
 
-## ??
+## mary jest Person(Mary)
 mary = Person("Mary")
 
-## ??
+## mary ma pet (satan
 mary.pet = satan 
 
-## ?? Ustaw frank na instancję klasy Employee 
+## frank jest Employee (Frank, 120000) 
 frank = Employee("Frank", 120000)
 
-## ?? Z frank weź atrybut pet i ustaw go na rover
+## frank ma pet (rover)
 frank.pet = rover
 
-## ?? Ustaw flipper na instancję klasy Fish
+## flipper jest Fish 
 flipper = Fish()
 
-## ?? Ustaw crouse na instancję klasy Salmon
+## crouse jest Salmon
 crouse = Salmon()
 
-## ?? Ustaw harry na instancję klasy Halibut
+## hary jest Halibut 
 harry = Halibut()
 
